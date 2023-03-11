c5cc8c14808935ee04497a8c183e72f499422b96
Robert Velasco <robertvelasco@Roberts-MacBook-Pro.local>
2023-03-11 11:14:59 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function MyButton({label, onClick}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="CalcButton" onClick ={onClick}>
+      {label}
+    </button>
   );
 }
 
-export default App;
+function Display({display}) {
+  return (
+    <div className="CalcDisplay">
+      {display}
+    </div>    
+  );
+}
+
+export default function App() {
+
+  const numberClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    console.log(value);
+    alert(value);
+
+  }
+
+  const operatorClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    console.log(value);
+    alert(value);
+
+  }
+
+  const equalClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    console.log(value);
+    alert(value);
+
+  }
+
+  const clearClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    console.log(value);
+    alert(value);
+
+  }
+
+
+  return (
+    <div className="App">
+      <h1>Welcome to my app</h1>
+      <Display display={0} />
+      <div className="ButtonContainer">
+        <MyButton label={7} onClick={numberClickHandler}/>
+        <MyButton label={8} onClick={numberClickHandler}/>
+        <MyButton label={9} onClick={numberClickHandler}/>
+        <MyButton label={"+"} onClick={operatorClickHandler}/>
+        <MyButton label={4} onClick={numberClickHandler}/>
+        <MyButton label={5} onClick={numberClickHandler}/>
+        <MyButton label={6} onClick={numberClickHandler}/>
+        <MyButton label={"-"} onClick={operatorClickHandler}/>
+        <MyButton label={1} onClick={numberClickHandler}/>
+        <MyButton label={2} onClick={numberClickHandler}/>
+        <MyButton label={3} onClick={numberClickHandler}/>
+        <MyButton label={"*"} onClick={operatorClickHandler}/>
+        <MyButton label={"C"} onClick={clearClickHandler}/>
+        <MyButton label={0} onClick={numberClickHandler}/>
+        <MyButton label={"="} onClick={equalClickHandler}/>
+        <MyButton label={"÷"} onClick={operatorClickHandler}/>
+      </div>
+    </div>
+  );
+}
